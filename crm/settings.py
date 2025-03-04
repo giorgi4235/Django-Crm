345c420683b2e2aebea49e869c1f4776642b454a
giorgi4235 <giorgi.chincharauli.1@btu.edu.ge>
2025-03-04 19:24:03 +0400

    'registration'

--- crm/settings.py
+++ crm/settings.py
@@ -1,11 +1,11 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-fm!rfd$t^*3ks*x_!gao0=%%2+ws)%ltfsy@kt(3_r8#_%y%*='
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'crm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
